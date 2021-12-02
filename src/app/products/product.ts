bb2f7e27f2efa828a73837513b8e02c2f8bf8ad7
Sergio Moreno <sergioams95@gmail.com>
2021-12-01 20:26:04 -0500

    prueba dist

--- src/app/products/product.ts
+++ src/app/products/product.ts
@@ -1,12 +1,12 @@
 export interface Product {
     id: string;
-    categoryId: number;
-    productName: string;
+    categoryid: number;
+    productname: string;
     description: string;
     rating: string;
     price: number;
     productImg: string;
-    isAvailable: boolean;
+    isavailable: boolean;
     color: string;
     reviews: number;
 }
